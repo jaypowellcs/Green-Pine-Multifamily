b62b13f2b2aa153a2246c6667871b1960cb3887a
Jay Powell <jay.powellcs@gmail.com>
2022-05-15 18:47:42 -0500

    Header & HomePage

---
+++ src/components/Header.js
@@ -0,0 +1,81 @@
+import { 
+    Row, 
+    Col, 
+    Nav,
+    NavItem, 
+    NavLink,  
+    DropdownItem, 
+    DropdownMenu, 
+    DropdownToggle, 
+    UncontrolledDropdown,
+    Navbar
+    } from 'reactstrap';
+
+const Header = () => {
+    return (
+        <Navbar color='success' sticky='top' expand='md'>
+            <Row>
+                <Col>
+                <Nav>
+                    <NavItem>
+                        <NavLink active href="#">Amenities 
+                        </NavLink>
+                    </NavItem>
+
+                    <UncontrolledDropdown>
+                        <DropdownToggle caret>Floor Plans 
+                        </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem >1 Bedrooms</DropdownItem>
+                                <DropdownItem >2 Bedrooms</DropdownItem>
+                                <DropdownItem>3 Bedrooms</DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>Townhomes</DropdownItem>
+                            </DropdownMenu>
+                    </UncontrolledDropdown>
+                   
+                    <NavItem>
+                        <NavLink  href="#">
+                            Photos
+                        </NavLink>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLink  href="#">
+                            Neighborhood
+                        </NavLink>
+                    </NavItem>
+
+                    <UncontrolledDropdown>
+                        <DropdownToggle caret>FAQ
+                        </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem >Applicant FAQ</DropdownItem>
+                                <DropdownItem >Utilities and Schools</DropdownItem>
+                                <DropdownItem>Interactive Room</DropdownItem>
+                                <DropdownItem>Renters Insurance</DropdownItem>
+                            </DropdownMenu>
+                    </UncontrolledDropdown>
+
+                    <NavItem>
+                        <NavLink  href="#">
+                            Schedule a Tour 
+                        </NavLink>
+                    </NavItem>
+
+                    <UncontrolledDropdown>
+                        <DropdownToggle caret>Contact Us
+                        </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem >Residents</DropdownItem>
+                                <DropdownItem >Future Investors</DropdownItem>
+                            </DropdownMenu>
+                    </UncontrolledDropdown>
+                    </Nav>
+                </Col>
+            </Row>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ src/pages/homePage.js
@@ -0,0 +1,12 @@
+import {Container} from 'reactstrap';
+
+
+const HomePage = () => {
+    return (
+        <Container>
+            <div>Hello</div>
+        </Container>
+    );
+};
+
+export default HomePage
